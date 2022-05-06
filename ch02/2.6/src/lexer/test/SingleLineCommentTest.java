12181749dbd8e2bdc0fc2bc9d1abc7eb2986656d
Evgenii Iuliugin <evgeny.yulyugin@intel.com>
2022-05-06 11:30:17 +0200

    Add tests for sinble line comments

---
+++ ch02/2.6/src/lexer/test/SingleLineCommentTest.java
@@ -0,0 +1,58 @@
+package lexer.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import org.junit.Test;
+
+import lexer.Lexer;
+import lexer.Word;
+import lexer.Token;
+import lexer.Tag;
+
+public class SingleLineCommentTest {
+    @Test
+    public void testSingleLineComment() throws IOException {
+        final String id = "someId";
+        final String comment = "// Random comment";
+        final String input = comment + "\n" + id;
+        Lexer lexer = new Lexer();
+        System.setIn(new ByteArrayInputStream(
+                        input.getBytes()));
+        Token token = lexer.scan();
+        assertEquals(2, lexer.line);
+        assertEquals(Tag.ID, token.tag);
+        Word idToken = (Word)token;
+        assertEquals(idToken.lexeme, id);
+    }
+
+    @Test
+    public void testTwoSingleLineComments() throws IOException {
+        final String id = "someId";
+        final String comment = "// Random comment";
+        final String input = comment + "\n" + comment + '\n' + id;
+        Lexer lexer = new Lexer();
+        System.setIn(new ByteArrayInputStream(
+                        input.getBytes()));
+        Token token = lexer.scan();
+        assertEquals(3, lexer.line);
+        assertEquals(Tag.ID, token.tag);
+        Word idToken = (Word)token;
+        assertEquals(idToken.lexeme, id);
+    }
+
+    @Test
+    public void testMisformedComment() throws IOException {
+        final String comment = "/misformed comment";
+        final String input = comment + "\n" + "doesntMatter";
+        Lexer lexer = new Lexer();
+        System.setIn(new ByteArrayInputStream(
+                        input.getBytes()));
+        Token token = lexer.scan();
+        assertEquals(1, lexer.line);
+        assertNotEquals(Tag.ID, token.tag);
+    }
+}

ace0b172d40897e85fcad76b8722addefa8e115b
Evgenii Iuliugin <yulyugin@gmail.com>
2025-10-23 09:06:43 +0200

    Add license to C++ files

--- ch02/2.4/src/2.4.1b.cpp
+++ ch02/2.4/src/2.4.1b.cpp
@@ -1,8 +1,33 @@
-/* Copyright (C) 2022 Evgenii Iuliugin
- *
+/*
  * Construct recursive-descent parsers, starting with the following grammars:
  * S -> S ( S ) S | epsilon
  *
+ * Copyright (C) 2022 Evgenii Iuliugin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+ * SOFTWARE.
+*/
+
+#include <gtest/gtest.h>
+
+class SyntaxError : public std::exception {};
+
+/*
  * The given grammar cannot be used as is, because it is left recursive.
  * Recursive-descent parser loops forever if applied for left recursive grammar.
  *
@@ -15,11 +40,6 @@
  * Result for the given translation:
  * S -> ( S ) S | epsilon
  */
-
-#include <gtest/gtest.h>
-
-class SyntaxError : public std::exception {};
-
 class Parser {
     char lookahead;
     std::stringstream parse_string;

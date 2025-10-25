0ef08b7495672c2867a918df802f429b8fd19400
Evgenii Iuliugin <yulyugin@gmail.com>
2025-10-25 11:24:57 +0200

    Add cmake rules for latex files

--- cmake/tex.cmake
+++ cmake/tex.cmake
@@ -1,3 +1,4 @@
+
 # Copyright (c) 2019 Evgenii Iuliugin
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -18,6 +19,32 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-add_subdirectory(2.2)
-add_subdirectory(2.4)
-add_subdirectory(2.6)
+option(BUILD_IMAGES "Build TeX images" OFF)
+
+if(BUILD_IMAGES)
+    find_package(LATEX COMPONENTS PDFLATEX)
+    find_program(PDF2SVG pdf2svg REQUIRED)
+    set(IMG_DIR ${CMAKE_BINARY_DIR}/img)
+
+    if(NOT LATEX_FOUND)
+        message(FATAL_ERROR "TeX not found.")
+    endif()
+
+    function(build_tex_image TEX_FILE)
+        set(TEX_OPTS "-interaction=nonstopmode")
+        execute_process(
+            COMMAND ${PDFLATEX_COMPILER} ${TEX_OPTS} ${TEX_FILE} -output-directory=${IMG_DIR}
+            COMMAND_ERROR_IS_FATAL ANY
+        )
+        get_filename_component(TEX_NAME ${TEX_FILE} NAME_WLE)
+        string(REGEX REPLACE "[.]tex$" ".svg" SVG_FILE ${TEX_FILE})
+        execute_process(
+            COMMAND ${PDF2SVG} "${IMG_DIR}/${TEX_NAME}.pdf" "${SVG_FILE}"
+            COMMAND_ERROR_IS_FATAL ANY
+        )
+    endfunction()
+else()
+    function(build_tex_image TEX_FILE)
+        message(STATUS "Skipping image generation for ${TEX_FILE}")
+    endfunction()
+endif()
